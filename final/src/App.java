e380f401309dbfcdb3d6138180dedce19a991be2
RuthDooley <61741718+RuthDooley@users.noreply.github.com>
2024-09-26 00:48:50 +0100

    Add files via upload

---
+++ final/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // Randomise the position, needs to be black and white of 1 king, 1 queen, 2 rooks, 2 bishops, 2 knights
+
+        // Start the loop for the game
+
+        // Upon first move start the timer
+
+        // As the user to give the position of the piece they want to move, and the position they want to move it to
+
+        // Check if this is a valid entry 
+
+        // Check if this is a valid move
+
+        // Move the piece 
+
+        // Check if game is over and if not continue
+    }
+}
